c12be3a760ad9fb3b55e7bdf50d28fee81688675
interdigitize <interdigitize@users.noreply.github.com>
2017-08-02 18:41:13 -0700

    Add database repo schema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: String,
+  html_url: String,
+  description: String,
+  updated_at: String,
+  watchers_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
